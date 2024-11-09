2da3284881a454ddfeb69f21ac0afb0807d517a5
Nurdin <mrsunnysummer@gmail.com>
2024-11-10 05:28:01 +0700

    update: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
+
     const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
     app.useLogger(logger);
     await app.listen(5000);
